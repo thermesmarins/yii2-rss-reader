662fc80a1a8cbfbbd41700e1978ea121471c45b2
Phil Sanders <philsanders79@gmail.com>
2015-07-21 14:21:42 -0400

    New iteration of item.php with css styles

--- views/item.php
+++ views/item.php
@@ -4,12 +4,25 @@ use yii\helpers\Html;
 use yii\i18n\Formatter;
 
 ?>
-<div class="well">
-	<div class="h3"><?php echo $model->title; ?></div>
-
-	<div><?php echo Yii::$app->formatter->asDatetime($model->published); ?></div>
-
-	<div>&nbsp;&nbsp;&nbsp;<?php echo $model->content; ?></div>
-
-	<div><?php echo Html::a(Yii::t('news', 'Read More...'), $model->link[0]['href']['0'], ['target' => '_new'] ); ?></div>
+<div class="well clearfix">
+	<h2 class="article-title">
+        <a href="<?php echo $model->link[0]['href']['0'] ?>"><?php echo $model->title; ?></a>
+    </h2>
+	<div class="article-meta">
+        <span class="article-date">
+            <?php echo Yii::$app->formatter->asDatetime($model->published); ?>
+        </span>
+    </div>
+	<div class="article-content">
+        <?php echo $model->content; ?>
+    </div>
+	<div class="article-footer">
+        <?php echo Html::a(Yii::t('news', 'Read More...'),
+              $model->link[0]['href']['0'],
+              [
+                'target' => '_new',
+                'class' => 'btn btn-primary pull-right'
+              ] );
+        ?>
+    </div>
 </div>
