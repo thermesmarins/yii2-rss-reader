6886ec8e2296fe81510c6fca00d9d7526366a890
Nico Mollet <nico.mollet@gmail.com>
2022-01-31 12:06:25 +0100

    Item now uses correct properties

--- src/views/item.php
+++ src/views/item.php
@@ -5,21 +5,21 @@ use yii\helpers\Html;
 ?>
 <div class="well clearfix">
 	<h2>
-        <a href="<?php echo $model->link[0]['href']['0'] ?>" target="_new"><?php echo $model->title; ?></a>
-    </h2>
+		<a href="<?php echo $model->link ?>" target="_new"><?php echo $model->title; ?></a>
+	</h2>
 	<p class="text-muted">
-        <?php echo Yii::$app->formatter->asDatetime($model->published); ?>
-    </p>
+		<?php echo Yii::$app->formatter->asDatetime( $model->pubDate ); ?>
+	</p>
 	<p>
-        <?php echo $model->content; ?>
-    </p>
+		<?php echo $model->content; ?>
+	</p>
 	<p>
-        <?php echo Html::a(Yii::t('news', 'Read More...'),
-              $model->link[0]['href']['0'],
-              [
-                'target' => '_new',
-                'class' => 'btn btn-primary pull-right'
-              ] );
-        ?>
-    </p>
+		<?php echo Html::a( Yii::t( 'rss', 'Read More...' ),
+			$model->link,
+			[
+				'target' => '_new',
+				'class'  => 'btn btn-primary pull-right',
+			] );
+		?>
+	</p>
 </div>
