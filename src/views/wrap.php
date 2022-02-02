9bfb480ab8560a82816b611aa7dea7efb3192175
Nico Mollet <nico.mollet@gmail.com>
2022-02-02 10:01:46 +0100

    Update views

--- src/views/wrap.php
+++ src/views/wrap.php
@@ -3,14 +3,14 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 
-$content = ListView::widget([
+echo ListView::widget( [
 	'dataProvider' => $dataProvider,
 	'itemView'     => $this->context->itemView,
-	'layout'       => "{items} \n {pager}"
-]);
-
-echo Html::tag(
-	$this->context->wrapTag,
-	$content,
-	['class' => $this->context->wrapClass]
-);
+	'options'      => $this->context->options,
+	'itemOptions'  => $this->context->itemOptions,
+	'options'      => [
+		'class' => '',
+		'tag'   => null,
+	],
+	'layout'       => "{items}",
+] );
